7f200e4b63145a3ad95e60476ecda80859692c4f
jacobkdick <jacobkdick@gmail.com>
2017-01-03 22:19:04 +0000

    Added favicon

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Dependencies
 const express       = require('express');
 const path          = require('path');
-const favicon       = require('serve-favicon');
 const logger        = require('morgan');
 const cookieParser  = require('cookie-parser');
 const bodyParser    = require('body-parser');
@@ -12,8 +11,6 @@ const app           = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,7 +22,7 @@ const home          = require('./routes/home');
 
 app.use('/', home);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
