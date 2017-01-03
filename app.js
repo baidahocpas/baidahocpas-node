ca6ffdfc3d122d0952dc914c1af1c0190936e927
jacobkdick <jacobkdick@gmail.com>
2017-01-03 21:18:24 +0000

    Fixed error referencing home route

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 var home          = require('./routes/home');
 
-app.use('/', index);
+app.use('/', home);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
